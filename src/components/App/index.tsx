d5b18d20fce027bed243b6af563a1f0516a81d6e
Gabriel Palladini <gmpalladini@gmail.com>
2020-10-08 11:14:02 +0200

    finished sttyling body

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NumberDisplay from "../NumberDisplay";
+import { generateCells } from "../../utils";
+import Button from "../Button";
 
 import "./App.scss";
 
 const App: React.FC = () => {
+
+    const [cells, setCells] = useState(generateCells()); 
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) => 
+        row.map((cell, colIndex) => 
+        <Button key={`${rowIndex}-${colIndex}`}/>)
+        ); 
+    };
 
     return (
         <div className='App'>
@@ -15,7 +26,7 @@ const App: React.FC = () => {
                     </span></div>
                 <NumberDisplay value={23} />
             </div>
-            <div className="Body">Body</div>
+            <div className="Body"> {renderCells()} </div>
         </div>
     )
 }
