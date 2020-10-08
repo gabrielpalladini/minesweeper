12e0564b9e447dc7bfcc13d303a45224f33303bb
Gabriel Palladini <gmpalladini@gmail.com>
2020-10-08 15:22:50 +0200

    finished logic cells

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import NumberDisplay from "../NumberDisplay";
 import { generateCells } from "../../utils";
 import Button from "../Button";
@@ -6,29 +6,37 @@ import Button from "../Button";
 import "./App.scss";
 
 const App: React.FC = () => {
+  const [cells, setCells] = useState(generateCells());
 
-    const [cells, setCells] = useState(generateCells()); 
+  console.log("cells", cells);
 
-    const renderCells = (): React.ReactNode => {
-        return cells.map((row, rowIndex) => 
-        row.map((cell, colIndex) => 
-        <Button key={`${rowIndex}-${colIndex}`}/>)
-        ); 
-    };
+  const renderCells = (): React.ReactNode => {
+    return cells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => 
+        <Button 
+            key={`${rowIndex}-${colIndex}`} 
+            state={cell.state}
+            value={cell.value}
+            row={rowIndex} 
+            col={colIndex}
+        />)
+    );
+  };
 
-    return (
-        <div className='App'>
-            <div className="Header"> 
-                <NumberDisplay value={0} />
-                <div className="Face">
-                    <span role="img" aria-label="face">
-                        😁
-                    </span></div>
-                <NumberDisplay value={23} />
-            </div>
-            <div className="Body"> {renderCells()} </div>
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">
+          <span role="img" aria-label="face">
+            😁
+          </span>
         </div>
-    )
-}
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body"> {renderCells()} </div>
+    </div>
+  );
+};
 
 export default App;
